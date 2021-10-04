63f3bc848a5a5c38ef8207cfb9f732755acd38a1
munvoseli <muslinmelody@gmail.com>
2021-10-04 15:59:23 -0400

    slightly better terrain generation

--- roomutils.c
+++ roomutils.c
@@ -1,10 +1,38 @@
 
+void apply_n_fractal( unsigned int n, Room_t* p_room )
+{
+	int x, y, r, x0, y0, x1, y1, x2, y2, sum;
+	for (int i = 0; i < n; ++i)
+	{
+		x = rand() % 38;
+		y = rand() % 38;
+		r = rand();
+		x0 = x + ((r     ) & 1) + ((r >> 1) & 1);
+		y0 = y + ((r >> 2) & 1) + ((r >> 3) & 1);
+		x1 = x + ((r >> 4) & 1) + ((r >> 5) & 1);
+		y1 = y + ((r >> 6) & 1) + ((r >> 7) & 1);
+		x2 = x + ((r >> 8) & 1) + ((r >> 9) & 1);
+		y2 = y + ((r >> 10) & 1) + ((r >> 11) & 1);
+		++x;
+		++y;
+		sum  = p_room->tiles[x0][y0];
+		sum += p_room->tiles[x1][y1];
+		sum += p_room->tiles[x2][y2];
+		// sum is 0, 1, 2, 3
+		p_room->tiles[x][y] = sum >> 1;
+		//if (p_room->tiles[x0][y0] == p_room->tiles[x1][y1])
+		//	p_room->tiles[x][y] = p_room->tiles[x0][y0];
+		//else
+		//	p_room->tiles[x][y] = p_room->tiles[x0][y0];
+	}
+}
+
 char generate_room( unsigned int id, Room_t* p_room )
 {
 	int i, j;
 	for (i = 0; i < Lx_room_tile; ++i)
 		for (j = 0; j < Ly_room_tile; ++j)
-			p_room->tiles[i][j] = rand() & 1;
+			p_room->tiles[i][j] = (rand() & 1) ? 0 : 1;
 	for (i = 0; i < Lx_room_tile; ++i)
 	{
 		p_room->tiles[i][0] = 1;
@@ -12,9 +40,10 @@ char generate_room( unsigned int id, Room_t* p_room )
 		p_room->tiles[0][i] = 1;
 		p_room->tiles[Lx_room_tile - 1][i] = 1;
 	}
-	for (i = 9; i < 31; ++i)
+	/*for (i = 9; i < 31; ++i)
 		for (j = 9; j < 31; ++j)
-		p_room->tiles[i][j] = 0;
+		p_room->tiles[i][j] = 0;*/
+	apply_n_fractal( 1600, p_room );
 	p_room->id = id;
 	j = rand() % 2 + 2;
 	for (i = 0; i < j; ++i)
